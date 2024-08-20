fa2ec498190f4c233a94ec105ec726050f8e2da5
Florent ATCHEAKOU <atcheakouf@gmail.com>
2024-08-20 22:52:35 +0200

    test sur le middleware à 100%

---
+++ src/tests/authMiddleware.test.js
@@ -0,0 +1,63 @@
+const authMiddleware = require('../middleware/authMiddleware');
+const axios = require('axios');
+jest.mock('axios');
+
+describe('Middleware d\'authentification', () => {
+  let req, res, next;
+
+  beforeEach(() => {
+    req = {
+      headers: {}
+    };
+    res = {
+      status: jest.fn(() => res),
+      send: jest.fn(),
+    };
+    next = jest.fn();
+  });
+
+  it('devrait retourner 403 si aucun token n\'est fourni', () => {
+    authMiddleware(req, res, next);
+    expect(res.status).toHaveBeenCalledWith(403);
+    expect(res.send).toHaveBeenCalledWith({ message: 'Aucun token fourni !' });
+  });
+
+  it('devrait retourner 500 si la vérification du token échoue', async () => {
+    req.headers['x-access-token'] = 'invalid_token';
+    axios.get.mockRejectedValue(new Error('Token error'));
+
+    await authMiddleware(req, res, next);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.send).toHaveBeenCalledWith({
+      message: 'Un problème est survenu lors de la vérification du token !',
+      error: 'Token error'
+    });
+  });
+
+  it('devrait appeler next si le token est valide', async () => {
+    req.headers['x-access-token'] = 'valid_token';
+    axios.get.mockResolvedValue({ status: 200 });
+
+    await authMiddleware(req, res, next);
+    expect(next).toHaveBeenCalled();
+  });
+
+  it('devrait retourner 401 si le token est expiré ou invalide', async () => {
+    req.headers['x-access-token'] = 'expired_token';
+    axios.get.mockResolvedValue({ status: 401 });
+  
+    await authMiddleware(req, res, next);
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.send).toHaveBeenCalledWith({
+      message: 'Échec de l\'authentification du token !'
+    });
+  });
+
+  it('devrait retourner 403 si le token est présent mais vide', () => {
+    req.headers['x-access-token'] = '';
+    authMiddleware(req, res, next);
+    expect(res.status).toHaveBeenCalledWith(403);
+    expect(res.send).toHaveBeenCalledWith({ message: 'Aucun token fourni !' });
+  });
+
+});
